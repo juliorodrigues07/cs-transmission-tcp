bb6a52e85947833be6d75a6f6000877ecdfe0418
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-12-16 23:44:23 -0300

    update: Nome do arquivo renomeado e chamadas novas bibliotecas

--- src/transmission.h
+++ src/transmission.h
@@ -1,5 +1,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
 #include <sys/wait.h>
 #include <pthread.h>
 #include <stdbool.h>
@@ -9,6 +11,8 @@
 #include <dirent.h>
 #include <stdio.h>
 
-// ACUSAÇÃO DE ERROS E CRIAÇÃO DAS CONEXÕES (SOCKETS)
-void error(const char *msg);
-int socket_creation(int port_number, int n_connections);
+// ACUSAÇÃO DE ERROS
+void error (const char *msg) {
+    perror(msg);
+    exit(1);
+}
