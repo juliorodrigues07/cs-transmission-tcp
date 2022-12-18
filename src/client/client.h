de70d5fbbc4a09e77dd2a604f0825fa17b61e4e1
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-12-18 14:29:59 -0300

    update: Removidas chamadas de bibliotecas não utilizadas

--- src/client/client.h
+++ src/client/client.h
@@ -2,13 +2,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
-#include <sys/wait.h>
-#include <pthread.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <dirent.h>
 #include <stdio.h>
 
 // ACUSAÇÃO DE ERROS
